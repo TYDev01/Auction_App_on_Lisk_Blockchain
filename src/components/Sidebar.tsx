3ce2cde13682910a546b153f766a7debd4352546
TYDev01 <anthonychinaemeru84@yahoo.com>
2025-05-02 13:27:06 +0100

    Few icons added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,16 @@
-
+import { FcDam, FcComboChart } from "react-icons/fc";
 
 
 export default function Sidebar(){
     return (
         <>
-        <div className="flex w-20 h-200 p-5 rounded-4xl bg-[#151945]/30 backdrop-blur-none ...">
-            
+        <div className="flex flex-col w-20 h-200 p-5 rounded-2xl border border-[#2b2e4f] bg-[#151945]/30 backdrop-blur-sm ...">
+            <div>
+                <FcDam  className="text-[2rem] mb-15 cursor-pointer"/>
+            </div>
+            <div>
+                <FcComboChart className="text-[2rem] cursor-pointer" />
+            </div>
         </div>
         </>
     )
