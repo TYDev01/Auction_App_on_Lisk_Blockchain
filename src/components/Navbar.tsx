3ab0a6ba1650795ae7de8f01d5f11c7f5feb63ed
TYDev01 <anthonychinaemeru84@yahoo.com>
2025-05-02 03:25:41 +0100

    Inital push

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+export default function Navbar(){
+    return (
+        <>
+        <div className="flex justify-end">
+            <a className="w-36 flex-initial p-3 rounded-4xl text-center ... bg-[#141a45] order-1 m-1">create</a>
+            <a className="w-40 flex-initial p-3 rounded-4xl text-center ... bg-[#141a45] order-2 m-1">connect wallet</a>
+        </div>
+        </>
+    )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Sidebar(){
+    return (
+        <>
+        <div className="flex w-20 h-200 p-5 rounded-4xl bg-[#151945]/30 backdrop-blur-none ...">
+            
+        </div>
+        </>
+    )
+}
+
